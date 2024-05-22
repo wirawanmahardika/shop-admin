7568d1c57a514b9f6ece98b7ae313536755149cc
wirawanmahardika <wiraprogrammer@gmail.com>
2024-05-22 15:25:48 +0800

    feat: menambah page lihat customers

--- src/pages/customer/Lihat.tsx
+++ src/pages/customer/Lihat.tsx
@@ -1,62 +1,60 @@
 export default function LihatCustomer() {
   return (
     <div className="w-full p-5 grid grid-cols-3 gap-5 overflow-y-auto ">
-      <span className="col-span-3 font-bold text-5xl text-center">Items</span>
-      <Item
-        nama="Baju Adidas Terbatas"
-        src="/img/baju.png"
-        id={2}
-        brand="Gucci"
-        price="200.000"
-        category="Baju"
+      <span className="col-span-3 font-bold text-5xl text-center">
+        Customers
+      </span>
+
+      <Customer
+        src="/img/hacker-2.jpg"
+        id={123}
+        fullname="Wirawan Mahardika"
+        username="wirawan"
+        email="wirawanmahardika10@gmail.com"
       />
-      <Item
-        nama="Kalung Cleopatra"
-        src="/img/kalung.png"
-        id={2}
-        brand="Adidas"
-        price="240.000"
-        category="Kalung"
+      <Customer
+        src="/img/hat1.jpg"
+        id={123}
+        fullname="The Hat"
+        username="hat"
+        email="hat123@gmail.com"
       />
-      <Item
-        nama="Gelang Arab"
-        src="/img/gelang.png"
-        id={2}
-        brand="Puma"
-        price="1.000.000"
-        category="Gelang"
+      <Customer
+        src="/img/hacker-2.jpg"
+        id={123}
+        fullname="Wirawan Mahardika"
+        username="wirawan"
+        email="wirawanmahardika10@gmail.com"
       />
     </div>
   );
 }
 
-function Item({
+function Customer({
   src,
-  nama,
   id,
-  brand,
-  price,
-  category,
+  fullname,
+  username,
+  email,
 }: {
-  price: string;
-  category: string;
-  brand: string;
   src: string;
-  nama: string;
   id: number;
+  fullname: string;
+  username: string;
+  email: string;
 }) {
   return (
     <div className="hover:bg-slate-600 p-5 flex flex-col gap-y-5 items-center border-2 border-black rounded-lg shadow-lg h-full justify-end">
-      <img className="max-w-[66%]" src={src} alt={nama} />
+      <img className="max-w-full" src={src} alt={fullname} />
       <div className="flex flex-col items-start w-full gap-y-3">
         <span className="font-bold text-4xl self-center text-center">
-          {nama}
+          {fullname}
         </span>
-        <div className="w-full grid grid-cols-2 gap-1 justify-items-start">
-          <span className="text-sm">Brand : {brand}</span>
+        <div className="w-full flex flex-col gap-y-2">
           <span className="text-sm">ID : {id}</span>
-          <span className="text-sm">Category : {category}</span>
-          <span className="text-sm">Price : Rp {price}</span>
+          <span className="text-sm">Fullname : {fullname}</span>
+          <span className="text-sm">Username : {username}</span>
+          <span className="text-sm">Email : {email}</span>
         </div>
       </div>
     </div>
