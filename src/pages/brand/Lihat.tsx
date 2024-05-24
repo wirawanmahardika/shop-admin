66a866f6ebf296f36109fd920b5163a18312fbdb
wirawanmahardika <wiraprogrammer@gmail.com>
2024-05-25 00:11:13 +0800

    chore: integrasi page lihat brand dengan backend

--- src/pages/brand/Lihat.tsx
+++ src/pages/brand/Lihat.tsx
@@ -1,25 +1,28 @@
+import useFetchGet from "../../hooks/useFetchGet";
+
 export default function LihatBrand() {
+  const brands = useFetchGet("/api/brands");
+
   return (
     <div className="w-full p-5 grid grid-cols-3 gap-5 overflow-y-auto ">
       <span className="col-span-3 font-bold text-5xl text-center">Brands</span>
-      <Category nama="Adidas" src="/img/adidas.png" id={2} />
-      <Category nama="Gucci" src="/img/gucci.png" id={3} />
-      <Category nama="Converse" src="/img/converse.png" id={4} />
-      <Category nama="Nike" src="/img/nike.png" id={5} />
-      <Category nama="Puma" src="/img/puma.png" id={5} />
+
+      {brands &&
+        brands.map((b) => {
+          return (
+            <Brand
+              key={b.id_brand}
+              nama={b.name_brand}
+              src={b.brand_photo}
+              id={b.id_brand}
+            />
+          );
+        })}
     </div>
   );
 }
 
-function Category({
-  src,
-  nama,
-  id,
-}: {
-  src: string;
-  nama: string;
-  id: number;
-}) {
+function Brand({ src, nama, id }: { src: string; nama: string; id: number }) {
   return (
     <div className="hover:bg-slate-600 p-5 flex flex-col gap-y-5 items-center border-2 border-black rounded-lg shadow-lg h-full justify-end">
       <img className="max-w-[66%]" src={src} alt={nama} />
