bcd044fb38bd6b1cf354b535596f8c95f9688046
wirawanmahardika <wiraprogrammer@gmail.com>
2024-05-26 22:15:59 +0800

    chore: mengfungsikan page edit brand dan category

--- src/pages/brand/Edit.tsx
+++ src/pages/brand/Edit.tsx
@@ -1,15 +1,38 @@
-import { Form } from "react-router-dom";
+import { useState } from "react";
+import { myAxios } from "../../helper/axios";
 
 export default function EditBrand() {
+  const [image, setImage] = useState<any>();
+  const [nama, setNama] = useState<string>("");
+  const [id, setID] = useState<string>("");
+
+  const submitEdit = async (e: any) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("id_brand", id);
+    formData.append("name_brand", nama);
+    formData.append("photo", image);
+
+    try {
+      const result = await myAxios.patch("/api/brands/edit-brand", formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-8 w-full flex flex-col gap-y-5 items-center">
       <span className="font-bold text-4xl ">Edit Brand</span>
-      <Form className="w-1/2 flex flex-col items-center gap-y-5">
+      <form className="w-1/2 flex flex-col items-center gap-y-5">
         <div className="flex flex-col w-full ">
           <label htmlFor="id_category" className="text-xl">
             ID Category
           </label>
           <input
+            onChange={(e) => setID(e.target.value)}
             type="text"
             className="w-full outline-none border-2 border-black rounded py-1"
           />
@@ -19,6 +42,7 @@ export default function EditBrand() {
             Nama
           </label>
           <input
+            onChange={(e) => setNama(e.target.value)}
             type="text"
             className="w-full outline-none border-2 border-black rounded py-1"
           />
@@ -28,12 +52,15 @@ export default function EditBrand() {
             Photo
           </label>
           <input
+            onChange={(e) => setImage(e.target.files?.[0])}
             type="file"
             className="w-full outline-none border-2 border-black rounded py-1"
           />
         </div>
-        <button className="px-8 py-1 rounded bg-sky-600">Submit</button>
-      </Form>
+        <button onClick={submitEdit} className="px-8 py-1 rounded bg-sky-600">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
--- src/pages/category/Edit.tsx
+++ src/pages/category/Edit.tsx
@@ -1,15 +1,45 @@
-import { Form } from "react-router-dom";
+import { useState } from "react";
+import { myAxios } from "../../helper/axios";
 
 export default function EditCategory() {
+  const [image, setImage] = useState<any>();
+  const [nama, setNama] = useState<string>("");
+  const [id, setID] = useState<string>("");
+
+  const submitEdit = async (e: any) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("id_category", id);
+    formData.append("category", nama);
+    formData.append("photo", image);
+
+    try {
+      const result = await myAxios.patch(
+        "/api/category/edit-category",
+        formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="p-8 w-full flex flex-col gap-y-5 items-center">
       <span className="font-bold text-4xl ">Edit Category</span>
-      <Form className="w-1/2 flex flex-col items-center gap-y-5">
+      <form
+        onSubmit={submitEdit}
+        className="w-1/2 flex flex-col items-center gap-y-5"
+      >
         <div className="flex flex-col w-full ">
           <label htmlFor="id_category" className="text-xl">
             ID Category
           </label>
           <input
+            onChange={(e) => setID(e.target.value)}
             type="text"
             className="w-full outline-none border-2 border-black rounded py-1"
           />
@@ -19,6 +49,7 @@ export default function EditCategory() {
             Nama
           </label>
           <input
+            onChange={(e) => setNama(e.target.value)}
             type="text"
             className="w-full outline-none border-2 border-black rounded py-1"
           />
@@ -28,12 +59,13 @@ export default function EditCategory() {
             Photo
           </label>
           <input
+            onChange={(e) => setImage(e.target.files?.[0])}
             type="file"
             className="w-full outline-none border-2 border-black rounded py-1"
           />
         </div>
         <button className="px-8 py-1 rounded bg-sky-600">Submit</button>
-      </Form>
+      </form>
     </div>
   );
 }
